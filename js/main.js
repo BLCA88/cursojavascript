863b46778151b1b157af8a88cdacd532fb380094
Jose Antunez <jl.antunez88@gmail.com>
2022-09-12 17:57:28 -0300

    Creando funcion y modificando DOM

--- js/main.js
+++ js/main.js
@@ -62,7 +62,9 @@ function crearCard() {
 
 let listaVocales = [] // aun no lo use pero lo voy a necesitar para crear unas tareas
 
-//Funcion de tareas completas para que utilice el Si y el No y aprenda lenguaje (La funcion va recorriendo cada elemento del array consultando si se realizo y ademas, separa las letras para compararlas con las vocales.)
+/* Funcion de tareas completas para que utilice el Si y el No y aprenda lenguaje (La funcion va recorriendo cada elemento del array consultando si se
+ realizo, crea con funcion CrearCard una estructura en DOM, crea elementos h2 y p dentro de cada div y separa las letras para compararlas con las vocales.) */
+ 
 function tareasCompletas (array) {
     for (let i = 0; i < array.length; i++) {                
         let arrayVocales = []        
@@ -87,18 +89,17 @@ function tareasCompletas (array) {
             for (const elemento of vocales) {
                 let resultadoFilter = letras.filter(letra => letra === elemento)
                 if (resultadoFilter[0] != undefined) {
-                    arrayVocales.push(resultadoFilter[0])
-                    console.log(resultadoFilter)
-                    let parrafoVocales = document.createElement('p')
-                    parrafoVocales.classList.add('p')
-                    parrafoVocales.innerText = resultadoFilter
-                    divBack.append(parrafoVocales)                   
+                    arrayVocales.push(resultadoFilter[0])              
                 }                                                             
             }                                                           
             listaVocales.push(arrayVocales)
+            let parrafoVocales = document.createElement('p')
+            parrafoVocales.classList.add('p')
+            parrafoVocales.innerText = arrayVocales
+            divBack.append(parrafoVocales)            
             let consulta = prompt('Escribe las vocales que tiene ' + array[i])
             if (consulta == arrayVocales) {                
-                alert ('Muy bien Benja, la respuesta es: ' + arrayVocales)                
+                alert ('Muy bien Benja, la respuesta es: ' + arrayVocales)                                
             }else                
                 alert ('Incorrecto, la respuesta correcta es ' + arrayVocales)                            
         }else if (tareaCompleta != 'Si' && tareaCompleta != 'si'){
@@ -110,14 +111,12 @@ function tareasCompletas (array) {
     return alert('Muy bien Benja!!! las tareas ' + array + ' estan completas.')            
 }
 
-//Condicion con horarios para que aprenda a usar las palabras y leer.
-let contador = 3
-//while (hora >= 10 && contador <= 3) {      
-    if (hora >= 10 && hora <= 11 && contador === 0) {
+//Funcion con setTime y horarios para que aprenda a usar las palabras y leer.
+function tareas () {     
+    if (hora >= 10 && hora <= 11) {
         alert (`Tenes que completar estas tareas para poder jugar con la tablet: 
 01: ${tareasMañana[0]} 02: ${tareasMañana[1]} 03: ${tareasMañana[2]} 04: ${tareasMañana[3]}`)
-        tareasCompletas(tareasMañana)                                
-        contador = contador + 1
+        tareasCompletas(tareasMañana)       
         let vestir = prompt(`Que remera y pantalon te gustaria usar hoy?
         ${vestimenta[0]}
         ${vestimenta[1]}
@@ -125,28 +124,34 @@ let contador = 3
         ${vestimenta[4]}        
         `)
         alert (vestir)
-    }else if (hora >= 12 && hora <= 14 && contador === 1) {
+        contador = contador + 1
+        setTimeout(tareas, 10800000)
+    }else if (hora >= 13  && hora <= 14) {
         alert (`Tenes que completar estas tareas para poder jugar con la tablet: 
 01: ${tareasMedia[0]} 02: ${tareasMedia[1]} 03: ${tareasMedia[2]} 04: ${tareasMedia[3]}`)
-        tareasCompletas(tareasMedia)                
-        contador = contador + 1       
-    }else if (hora >= 16 && hora <= 19 && contador === 2) {
+        tareasCompletas(tareasMedia)
+        contador = contador + 1
+        setTimeout(tareas, 10800000)
+    }else if (hora >= 16 && hora <= 18) {
         alert (`Tenes que completar estas tareas para poder jugar con la tablet: 
 01: ${tareasTarde[0]} 02: ${tareasTarde[1]} 03: ${tareasTarde[2]}`)
-        tareasCompletas(tareasTarde)               
+        tareasCompletas(tareasTarde)
         contador = contador + 1
-    }else if (hora >= 20 && contador === 3) {
+        setTimeout(tareas, 14400000)
+    }else if (hora >= 20) {
         alert (`Ultimas tareas antes de ir a dormir: 
 01: ${tareasNoche[0]} 02: ${tareasNoche[1]} 03: ${tareasNoche[2]} 04: ${tareasNoche[3]} 05: ${tareasNoche[4]}`)
-        tareasCompletas(tareasNoche)                
-        contador = 0 
+        tareasCompletas(tareasNoche)     
         let vestir = prompt(`Que te gustaria ponerte para dormir?
         ${vestimenta[0]}
         ${vestimenta[1]}
         ${vestimenta[2]}
         ${vestimenta[4]}        
         `)
-        alert (vestir)             
-    }  
-//}
-alert ('Buenas Noches Benja')
+        alert (vestir)
+        alert ('Buenas Noches Benja')
+        contador = 0
+        setTimeout(tareas, 50400000)                           
+    }
+}
+setTimeout(tareas, 1000)
