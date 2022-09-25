3bc4a2e0696abcf987127f58d022ed44975adc4c
Jose Antunez <jl.antunez88@gmail.com>
2022-09-25 19:15:04 -0300

    Creando modal y guardando eventos en localStorage

--- js/main.js
+++ js/main.js
@@ -1,53 +1,10 @@
 //Aprendizaje rutina, palabras, numeros y hora
-
 //Arrays con tareas que debe realizar para poder utilizar la tablet
 const tareasMañana = ['Levantarse', 'Baño', 'Dientes', 'Desayuno']
 const tareasMedia = ['Almuerzo', 'Dientes', 'Trampolin', 'Picar']
 const tareasTarde = ['Merienda', 'Jugar', 'Bañarse']
 const tareasNoche = ['Cenar', 'Jugar', 'Ordenar', 'Pastilla', 'Baño']
 const vocales = ['a', 'e', 'i', 'o', 'u']
-
-//<----------<Eventos>------------>//
-let card = document.querySelectorAll('.containerFrontBack')
-let boton = document.querySelectorAll('.buton')
-let containerBoton = document.querySelectorAll('.divB')
-let containerFrontBack = document.querySelectorAll('.containerFrontBack')
-let containerFront = document.querySelectorAll('.containerFront')
-let h2 = document.querySelectorAll('.h2')
-
-//Evento en tarjeta para mostrar el completado cuando se preiona el boton SI
-function ocultarDiv (element){
-    let divClickeado = element.parentElement
-    divClickeado.classList.add("visibilityNone")
-    divClickeado.lastElementChild.classList.remove("visibilityNone")
-    divClickeado.lastElementChild.classList.add("visibilityYes")
-}
-
-boton.forEach(element => {       
-    if (element.name === 'si') {
-        element.addEventListener('click', mensaje1)
-        element.addEventListener('click', function(){ocultarDiv(element)})     
-    } else if (element.name === 'no') {
-        element.addEventListener('click', mensaje2)
-    }
-    function mensaje1() {    
-        alert('Muy bien Benja')                
-    }
-    function mensaje2() {    
-        alert('Tenes que completar la tarea')       
-    }      
-})
-// Funcion con evento para mostrar tarjetas
-function mostrarDiv (array) {
-    for (let contenedor of containerFront) {
-        let variable = contenedor.firstElementChild.innerHTML        
-        for (let elemento of array) {
-            if (variable == elemento){
-                contenedor.parentElement.classList.remove('displayNone')                                
-            } 
-        }            
-    }    
-}
 
 //funcion para agregar el 0 en hora, minutos y segundos
 function checkTime(i) {
@@ -56,9 +13,9 @@ function checkTime(i) {
     }
     return i
 }
-//Fecha en español para que aprenda los dias, meses y años, ademas la hora y minutos.
+//Fecha en español para que aprenda los dias, meses y años, hora y minutos.
 function fechaHora() {
-    let fecha = new Date()
+    const fecha = new Date()
     let [hora, minutos, segundos] = [fecha.getHours(), fecha.getMinutes(), fecha.getSeconds()]                
     hora = checkTime(hora)    
     minutos = checkTime(minutos)
@@ -73,15 +30,7 @@ function fechaHora() {
     let date = diaActual+", "+fechaActual+" "+mesActual+" "+añoActual    
     document.getElementById("fecha").innerHTML = date
     let refresh = setTimeout(function(){fechaHora()}, 500)
-    if (hora >= 10 && hora <= 12) {        
-        tareasCompletas(tareasMañana)
-    }else if (hora >= 13  && hora <= 14) {        
-        tareasCompletas(tareasMedia)
-    }else if (hora >= 16 && hora <= 18) {
-        tareasCompletas(tareasTarde)
-    }else if (hora >= 20) {
-        tareasCompletas(tareasNoche)                         
-    }          
+    return (hora, minutos, segundos)                 
 }
 //Para llamar automaticamente la funcion fechaHora
 const llamar = function (callback) {
@@ -89,44 +38,144 @@ const llamar = function (callback) {
 } 
 llamar(fechaHora)
 
-//Array con objetos de vestimenta
-let ropero = [ 
-    {id: 1, material: 'algodon', tipo: 'remera manga corta', color: 'azul', diseño: 'ositos estampados'},
-    {id: 2, material: 'algodon', tipo: 'remera manga larga', color: 'azul francia', diseño: 'liso'},
-    {id: 3, material: 'polyester', tipo: 'pantalon largo liviano', color: 'verde, azul', diseño: 'cuadrille'},
-    {id: 3, material: 'algodon', tipo: 'pantalon largo liviano', color: 'rosa, verde nilo, celeste', diseño: 'cuadrille'},
-    {id: 4, material: 'seda organica', tipo: 'pantalon corto', color: 'rojo', diseño: 'liso'},
-    {id: 4, material: 'polyester', tipo: 'pantalon corto', color: 'blanco, verde', diseño: 'dividido'},
-    {id: 5, material: 'algodon', tipo: 'calzoncillo', color: 'negro', diseño: 'liso'},
-    {id: 5, material: 'algodon', tipo: 'calzoncillo', color: 'gris', diseño: 'liso'},
-    {id: 6, material: 'cuero sintetico', tipo: 'zapatilla', color: 'negro', diseño: 'puma'}
-]
-let vestimenta = ropero.map(function(ropa){  // Filtra los tipo de ropas para consultar mas adelante que quiere vestir especificamente.  
-    return (`${ropa.tipo} ${ropa.color}`)
-})
+//----------------------------------------------------------------------------------------------------------------------------------------------------//
+
+//<----------<Eventos>------------>//
+let boton = document.querySelectorAll('.buton')
+let containerBoton = document.querySelectorAll('.divB')
+let containerFrontBack = document.querySelectorAll('.containerFrontBack')
+let containerFront = document.querySelectorAll('.containerFront')
+let h2 = document.querySelectorAll('.h2')
+let modalSi = document.querySelector('.modalSi')
+let modalNo = document.querySelector('.modalNo')
+let cerrarModalSi= document.getElementById('Si')
+let cerrarModalNo= document.getElementById('No')
+
+
+
+// Funcion con evento para mostrar tarjetas
+function mostrarDiv (array) {
+    for (let contenedor of containerFront) {
+        let variable = contenedor.firstElementChild.innerHTML                
+        for (let elemento of array) {            
+            if (variable === elemento){
+                contenedor.parentElement.classList.remove('displayNone')                                                
+            } 
+        }            
+    }    
+}
 
 function tareasCompletas (array) {
-    for (let i = 0; i < array.length; i++) {                
-        let arrayVocales = []
-        const letras = array[i].split('')
-        mostrarDiv(array)                                       
-        /* if (tareaCompleta === 'Si' || tareaCompleta === 'si') {                                  
-            for (const elemento of vocales) {
-                let resultadoFilter = letras.filter(letra => letra === elemento)
-                if (resultadoFilter[0] != undefined) {
-                    arrayVocales.push(resultadoFilter[0])              
-                }                                                             
-            }                                                           
-            listaVocales.push(arrayVocales)           
-            if (consulta == arrayVocales) {                
-                alert ('Muy bien Benja, la respuesta es: ' + arrayVocales)                                
-            }else                
-                alert ('Incorrecto, la respuesta correcta es ' + arrayVocales)                            
-        } */
+    for (let i = 0; i < array.length; i++) {        
+        mostrarDiv(array)
     }
 }
 
+const fecha = new Date()
+let [hora, minutos, segundos] = [fecha.getHours(), fecha.getMinutes(), fecha.getSeconds()] 
 
 
+//Funcion con condicional para mostrar tarjetas segun hora.
+function horario (){           
+    if (hora == 10 && minutos == 00 && segundos == 0) {        
+        location.reload()
+        localStorage.clear()        
+    }else if (hora >= 10 && hora < 12 ) {       
+        tareasCompletas(tareasMañana)        
+    }else if (hora == 12 && minutos == 00 && segundos == 0){
+        location.reload()
+        localStorage.clear()
+    }else if (hora >= 12  && hora < 16) {
+        tareasCompletas(tareasMedia)                        
+    }else if (hora == 16 && minutos == 00 && segundos == 0) {       
+        location.reload()
+        localStorage.clear()
+    }else if (hora >= 16 && hora < 20){
+        tareasCompletas(tareasTarde)
+    }else if (hora == 20 && minutos == 00 && segundos == 0) {       
+        location.reload()
+        localStorage.clear()
+    }else if (hora >= 0) {
+        tareasCompletas(tareasNoche)                                
+    }
+    let refresh = setTimeout(function(){horario()}, 1100)
+}
+llamar(horario)
 
+//
+class ropero {
+    constructor(id, material, tipo, color, diseño){
+        this.id = id
+        this.material = material
+        this.tipo = tipo
+        this.color = color
+        this.diseño = diseño 
+    }
+}
+let divHtml = []
+
+boton.forEach(element => {           
+    if (element.name === 'si') {
+        element.addEventListener('click', () => {            
+            modalSi.classList.add('mostrarModal')            
+            element.parentElement.classList.add('visibilityNone')
+            element.parentElement.lastElementChild.classList.remove('visibilityNone')
+            element.parentElement.lastElementChild.classList.add('visibilityYes')
+            let propiedadPadre = element.parentElement            
+            if (propiedadPadre.classList.contains('visibilityNone')){
+                divHtml.push(propiedadPadre.offsetParent.outerHTML)
+                guardarLocal()                
+            }                             
+        })                      
+    } else if (element.name === 'no') {
+        element.addEventListener('click', () => {
+            modalNo.classList.add('mostrarModal')            
+        })
+    }          
+})
+cerrarModalSi.addEventListener('click', () => {
+    modalSi.classList.remove('mostrarModal')               
+})
+cerrarModalNo.addEventListener('click', () => {
+    modalNo.classList.remove('mostrarModal')               
+})
+
+//Funcion, ciclos y eventos para mantener el divHtml de las tarjetas  
+
+let infoLocal = []
+let domInicio = []
+
+for (let i = 0; i < localStorage.length; i++) {
+    infoLocal.push(localStorage.getItem(i))    
+}
+
+for (let i = 0; i < containerFront.length; i++) {
+    domInicio.push(containerFront[i])
+}
+
+for (let i = 0; i < infoLocal.length; i++) {
+    for (let e = 0; e < domInicio.length; e++) {  
+       let domInicioMin = (domInicio[e].firstElementChild.innerText).toLowerCase()       
+       let infoLocalMin = infoLocal[i].toLowerCase()       
+       if(infoLocalMin.includes(domInicioMin)) {            
+            domInicio[e].innerHTML = infoLocal[i]            
+       }
+    }
+}
+function guardarLocal () {
+    for (let i = 0; i < divHtml.length; i++) {
+        if (localStorage[0] == undefined) {
+            localStorage.setItem(i, divHtml[i])
+            divHtml = []
+        }else {            
+            for (let clave in Object.entries(localStorage)) {
+                let key = Number(clave) + 1                
+                if (localStorage[key] == undefined) {                                                         
+                    localStorage.setItem(key, divHtml[i])
+                    divHtml = []                  
+                }
+            }      
+        }
+    }
+}
 
